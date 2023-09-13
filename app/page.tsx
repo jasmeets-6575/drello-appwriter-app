d292773d1830b564a65310e7df3c9b292ed41981
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-09-13 15:52:36 +0530

    setting header

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
     <main>
+      {/* Header */}
+      <Header />
       <h1>Drello Ai Clone</h1>
     </main>
   );
