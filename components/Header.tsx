f530731862c4e7f5639e1099af2b8b771e6e38c8
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-09-13 16:23:04 +0530

    adding GPT  component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import logo from "@/public/logo.png";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import Avatar from "react-avatar";
 
 const Header = () => {
   return (
@@ -13,7 +16,8 @@ const Header = () => {
           height={100}
           className="w-44 md:w-56 pb-10 md:pb:0 object-contain"
         />
-        <div>
+
+        <div className="w-full flex items-center space-x-5 flex-1 justify-end">
           <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
             <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
             <input
@@ -25,8 +29,14 @@ const Header = () => {
               Search
             </button>
           </form>
+          {/* <Avatar name="Jasmeet Singh" round={true} color="#0055D1" size="50" /> */}
+        </div>
 
-          {/* Avatar */}
+        <div className="flex items-center justify-center px-5 md:py-5">
+          <p className=" flex items-center text-sm font-light shadow-xl rounded-xl p-5 w-fit bg-white italic max-w-3xl text-[#0055D1]">
+            <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
+            GPT is summarising your task for the day
+          </p>
         </div>
       </div>
     </header>
