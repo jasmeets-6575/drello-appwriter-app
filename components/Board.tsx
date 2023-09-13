e984a0b577979bf9f45ef319268a16da91f04a22
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-09-13 17:11:15 +0530

    DragDropContext

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,4 +1,12 @@
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
 const Board = () => {
-  return <div>Board</div>;
+  return (
+    <DragDropContext>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => <div>{/* rendering all the columns */}</div>}
+      </Droppable>
+    </DragDropContext>
+  );
 };
 export default Board;
