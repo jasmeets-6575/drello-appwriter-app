3b7066262497e52312ec42447f17bb363e38b29e
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-09-15 14:00:27 +0530

    setting rearranging column functionality

--- components/Board.tsx
+++ components/Board.tsx
@@ -6,16 +6,28 @@ import { useBoardStore } from "@/store/BoardStore";
 import Column from "./Column";
 
 const Board = () => {
-  const [board, getBoard] = useBoardStore((state) => [
+  const [board, getBoard, setBoardState] = useBoardStore((state) => [
     state.board,
     state.getBoard,
+    state.setBoardState,
   ]);
 
   useEffect(() => {
     getBoard();
   }, [getBoard]);
 
-  const handleOnDragEnd = (result: DropResult) => {};
+  const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source, type } = result;
+
+    if (!destination) return;
+    if (type === "column") {
+      const entries = Array.from(board.columns.entries());
+      const [removed] = entries.splice(source.index, 1);
+      entries.splice(destination.index, 0, removed);
+      const rearrangedColumns = new Map(entries);
+      setBoardState({ ...board, columns: rearrangedColumns });
+    }
+  };
 
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
