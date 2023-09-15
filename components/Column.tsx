3417fbd325f52ac4917430795053827567586a8a
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-09-15 13:16:32 +0530

    Ui setting

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,10 +1,18 @@
 import { Todo, TypedColumn } from "@/typings";
-import { Draggable } from "react-beautiful-dnd";
+import { Draggable, Droppable } from "react-beautiful-dnd";
 
 type Props = {
   id: TypedColumn;
   todos: Todo[];
   index: number;
+};
+
+const idToColumnText: {
+  [key in TypedColumn]: string;
+} = {
+  todo: "To Do",
+  inprogress: "In Progress",
+  done: "Done",
 };
 
 const Column = ({ id, todos, index }: Props) => {
@@ -16,7 +24,24 @@ const Column = ({ id, todos, index }: Props) => {
           {...provided.dragHandleProps}
           ref={provided.innerRef}
         >
-          {/* render droppable */}
+          <Droppable droppableId={index.toString()} type="card">
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className={`pb-2 p-2 rounded-2xl shadow-sm ${
+                  snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"
+                }`}
+              >
+                <h2 className="flex justify-between font-bold text-xl p-2">
+                  {idToColumnText[id]}
+                  <span className="text-gray-500 bg-gray-200 rounded-full font-normal px-2 py-1 text-sm">
+                    {todos.length}
+                  </span>
+                </h2>
+              </div>
+            )}
+          </Droppable>
         </div>
       )}
     </Draggable>
