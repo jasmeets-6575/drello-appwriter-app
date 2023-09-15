ee206289d4e9184e849944fc79b6b962c544a542
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-09-15 12:41:52 +0530

    sorting columns

--- lib/getTodosGroupedByColumn.ts
+++ lib/getTodosGroupedByColumn.ts
@@ -1,5 +1,5 @@
 import { databases } from "@/appwrite";
-import { Column, TypedColumn } from "@/typings";
+import { Board, Column, TypedColumn } from "@/typings";
 
 export const getTodosGroupedByColumn = async () => {
   const data = await databases.listDocuments(
@@ -26,4 +26,25 @@ export const getTodosGroupedByColumn = async () => {
     });
     return acc;
   }, new Map<TypedColumn, Column>());
+
+  const columnTypes: TypedColumn[] = ["todo", "inprogress", "done"];
+  for (const columnType of columnTypes) {
+    if (!columns.get(columnType)) {
+      columns.set(columnType, {
+        id: columnType,
+        todos: [],
+      });
+    }
+  }
+
+  const sortedColumns = new Map(
+    Array.from(columns.entries()).sort(
+      (a, b) => columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0])
+    )
+  );
+
+  const board: Board = {
+    columns: sortedColumns,
+  };
+  return board;
 };
