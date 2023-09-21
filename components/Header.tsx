07b70422f5204ab58318a76391b2323582496278
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-09-21 14:28:24 +0530

    done search functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,13 @@ import Image from "next/image";
 import logo from "@/public/logo.png";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Avatar from "react-avatar";
+import { useBoardStore } from "@/store/BoardStore";
 
 const Header = () => {
+  const [searchString, setSearchString] = useBoardStore((state) => [
+    state.searchString,
+    state.setSearchString,
+  ]);
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-50 rounded-b-2xl ">
@@ -27,6 +32,8 @@ const Header = () => {
             <input
               type="text"
               placeholder="Search"
+              value={searchString}
+              onChange={(e) => setSearchString(e.target.value)}
               className="flex-1 outline-none p-2"
             />
             <button type="submit" hidden>
